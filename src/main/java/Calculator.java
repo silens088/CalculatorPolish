17776d50ed10a2219d6424db4d02f774431da22b
Vitalii <silens088@gmail.com>
2020-04-02 17:49:29 +0300

    refactoring

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,34 +2,54 @@ import java.util.Stack;
 
 public class Calculator {
 
+    public static void main(String[] args) {
+        String expression = "2+2*2";
+        System.out.println(new Calculator().decide(expression));
+    }
+
+    public double decide(String expression) {
+        String rpn = ExpressionToRPN(expression);
+        return rpnToAnswer(rpn);
+    }
+
+    private String preparingExpression(String expression) {
+        String preparedExpression = new String();
+
+        for (int token = 0; token < expression.length(); token++) {
+            char symbol = expression.charAt(token);
+        }
+        return preparedExpression;
+    }
+
     //первый метод - выражение в обратную польскую нотацию
-    public static String ExpressionToRPN(String Expr) {
+    private String ExpressionToRPN(String expression) {
+
         String current = "";
         Stack<Character> stack = new Stack<Character>();
 
         int priority;
-        for (int i = 0; i < Expr.length(); i++) {
-            priority = getP(Expr.charAt(i));
+        for (int i = 0; i < expression.length(); i++) {
+            priority = getPriority(expression.charAt(i));
 
-            if (priority == 0) {
-                current += Expr.charAt(i);
-            }
-            if (priority == 1) {
-                stack.push(Expr.charAt(i));
-            }
+            if (priority == 0)
+                current += expression.charAt(i);
+
+            if (priority == 1)
+                stack.push(expression.charAt(i));
+
             if (priority > 1) {
                 current+=' ';
                 while (!stack.empty()) {    //пока стек не пустой проверяем следущее:
-                    if (getP(stack.peek()) >= priority) {  //если приоритет перхнего элемента нашего стека > or = приоритету текущего символа
+                    if (getPriority(stack.peek()) >= priority) {  //если приоритет перхнего элемента нашего стека > or = приоритету текущего символа
                         current+=stack.pop();  //то мы достаем этот символ из стека и кидаем его в current
                     } else
-                        break; //иначе завершаем цикл
+                        break;
                 }
-                stack.push(Expr.charAt(i));  //запихиваем в стек текущий символ
+                stack.push(expression.charAt(i));  //запихиваем в стек текущий символ
             }
             if (priority == -1) {
                 current+=' ';
-                while (getP(stack.peek()) != 1) {  //пока приоритет верхнего элемента стека не равен 1:
+                while (getPriority(stack.peek()) != 1) {  //пока приоритет верхнего элемента стека не равен 1:
                     current+=stack.pop();
                 }
                 stack.pop(); //pop() - Возвращает элемент, находящийся в верхней части стэка, удаляя его в процессе.
@@ -41,16 +61,17 @@ public class Calculator {
         return current;
     }
 
-    //обратную ПЗ переводим в ответ
-    public static double RPNtoAnswer(String rpn) {
+    //парсит польскую запись и переводит в ответ
+    private double rpnToAnswer(String rpn) {
         String operand = new String();
         Stack<Double> stack = new Stack<Double>();
 
         for(int i = 0; i < rpn.length(); i++) {
-            if (rpn.charAt(i) == ' ') continue; //если пробел, то запускаем заново
+            if (rpn.charAt(i) == ' ') //если пробел, то запускаем заново
+                continue;
 
-            if (getP(rpn.charAt(i)) == 0) { //если попался символ(число), то засовываем всё число в операнд.
-                while (rpn.charAt(i) != ' ' && getP(rpn.charAt(i)) == 0)
+            if (getPriority(rpn.charAt(i)) == 0) { //если попался символ(число), то засовываем всё число в операнд.
+                while (rpn.charAt(i) != ' ' && getPriority(rpn.charAt(i)) == 0)
                     operand+=rpn.charAt(i++);
                 if(i == rpn.length()) break;
 
@@ -61,7 +82,7 @@ public class Calculator {
 //            Object pop()
 //            Возвращает элемент, находящийся в верхней части стэка, удаляя его в процессе.
 
-            if(getP(rpn.charAt(i)) > 1) { //если приоритет это мат символ, то:
+            if(getPriority(rpn.charAt(i)) > 1) { //если приоритет это мат символ, то:
                 double a = stack.pop(), b = stack.pop(); //забрали два верхних числа
 
                 if(rpn.charAt(i) == '+') stack.push(b + a);
@@ -74,7 +95,7 @@ public class Calculator {
     }
 
     //метод определяющий приоритет
-    private static int getP(char token) {
+    private int getPriority(char token) {
         if (token == '*' || token == '/') {
             return 3;
         } else if (token == '+' || token == '-') {
